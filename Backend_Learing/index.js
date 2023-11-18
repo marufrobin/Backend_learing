4422fe635e676d31ceda93a741afe9d90314dd6c
Maruf Robin <marufrobin5@gmail.com>
2023-11-18 13:18:43 +0600

    read and write file done

--- Backend_Learing/index.js
+++ Backend_Learing/index.js
@@ -1,6 +1,23 @@
-const myName ="Robin";
-const firstName = "Md Maruf"
-const midelName = "Ahmed"
-const fullName =  `${firstName} ${midelName} ${myName}`;
-console.log(fullName);
+// const myName ="Robin";
+// const firstName = "Md Maruf"
+// const midelName = "Ahmed"
+// const fullName =  `${firstName} ${midelName} ${myName}`;
+// console.log(fullName);
 
+
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+//* * * * * * * Read and write from file * * * * * * * * * * * *  
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
+
+const fs = require('fs');
+var textIn = fs.readFileSync('./1-node-farm/starter/txt/read-this.txt',"utf-8");
+// console.log(textIn);
+
+const textout = `This is the test for: ${textIn}. \nCreated on ${Date.now()}`;
+// console.log(textout);
+
+fs.writeFileSync('./1-node-farm/starter/txt/output.txt',textout);
+
+console.log(textout);
+
+//* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
