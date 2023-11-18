a214f972aaf1130d9bc4a37f3b5c9d8e2743006f
Maruf Robin <marufrobin5@gmail.com>
2023-11-18 14:00:39 +0600

    practice file added

--- Backend_Learing/index.js
+++ Backend_Learing/index.js
@@ -9,15 +9,15 @@
 //* * * * * * * Read and write from file * * * * * * * * * * * *  
 //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
 
-const fs = require('fs');
-var textIn = fs.readFileSync('./1-node-farm/starter/txt/read-this.txt',"utf-8");
-// console.log(textIn);
+// const fs = require('fs');
+// var textIn = fs.readFileSync('./1-node-farm/starter/txt/read-this.txt',"utf-8");
+// // console.log(textIn);
 
-const textout = `This is the test for: ${textIn}. \nCreated on ${Date.now()}`;
+// const textout = `This is the test for: ${textIn}. \nCreated on ${Date.now()}`;
+// // console.log(textout);
+
+// fs.writeFileSync('./1-node-farm/starter/txt/output.txt',textout);
+
 // console.log(textout);
-
-fs.writeFileSync('./1-node-farm/starter/txt/output.txt',textout);
-
-console.log(textout);
 
 //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  
