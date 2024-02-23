0b340a280d8252f42e8e83ce313b639e8d978a9a
Maruf Robin <marufrobin5@gmail.com>
2024-02-23 16:59:29 +0600

    wip: String interpolation done✅ now primitive types

---
+++ JavaScript Learning/script.js
@@ -0,0 +1,25 @@
+// let js = "amazing";
+// console.log(js);
+// if (js == "amazing") {
+//   alert(js);
+// }
+
+// console.log(40 + 8 + 230 - 10);
+
+/*
+------------------------------------------------------
+let countryName = "🇧🇩 Bangladesh 🇧🇩";
+let continent = "South Asia";
+let population = "170";
+
+console.log(`Country name: ${countryName}`);
+console.log(`Continent name: ${continent}`);
+console.log(`Population: ${population}`);
+------------------------------------------------------
+*/
+
+//----------- Primitive data types ----------------------
+
+// 1.Number: Floating point numbers used for decimal and
+// integers
+let age = 23;
