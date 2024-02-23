016d7786825a671513bdef24ba798980a9f87e66
Maruf Robin <marufrobin5@gmail.com>
2024-02-23 17:27:29 +0600

    wip:✅ primitive data types

--- JavaScript Learning/script.js
+++ JavaScript Learning/script.js
@@ -23,3 +23,21 @@ console.log(`Population: ${population}`);
 // 1.Number: Floating point numbers used for decimal and
 // integers
 let age = 23;
+
+// 2. String: Sequence of character used for text
+let name = "Robin";
+
+// 3. Boolean: Logical type can only be true or false used for taking decisions
+let fullAged = true;
+
+// 4. Undefined: Value taken by a variable that is not yet defined('empty value')
+let children;
+
+// 5. Null: Also means 'empty value'
+
+// 6. Symbol(ES2015): Value that is unique and cannot be changed[not useful for now]
+
+// 7. BigInt (ES2020): larger integers than the Number type can hold
+
+// Note: JS has dynamic typing: we do not have to manually define the data type of the value stored in a variable
+// instead, data types are determined automatically
