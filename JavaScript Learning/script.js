154cb0508d5221486394e17163373e18922e6df2
Maruf Robin <marufrobin5@gmail.com>
2024-02-26 10:24:41 +0600

    type conversion

--- JavaScript Learning/script.js
+++ JavaScript Learning/script.js
@@ -317,3 +317,19 @@ if (BMIMark > BMIJohn) {
                          Type conversion and coercion
 --------------------------------------------------------------------------
 */
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear) + 18); //output: 2009
+console.log(inputYear + 18); // output: 199118
+
+console.log(Number("Robin"));
+console.log(typeof NaN);
+
+console.log(String("14"), 14);
+console.log(typeof String("14"), typeof 14);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
