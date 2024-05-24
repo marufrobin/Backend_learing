3c26908e49fc41afdb3802f46ca2e253bb2fb5f7
Maruf Robin <marufrobin5@gmail.com>
2024-05-24 10:19:17 +0600

    testing is on going

---
+++ JavaScript Learning/test.js
@@ -0,0 +1,72 @@
+// let number = 3;
+
+// function makeSquare(number) {
+//   return number * number;
+// }
+
+// const functionValue = makeSquare(number);
+// console.log(functionValue);
+
+// function describePerson(name, age, isStudent) {
+//   return `${name} is ${age} years old and is ${
+//     isStudent ? "a Student." : "not a student."
+//   }`;
+// }
+
+// let result = describePerson("John", 25, true);
+// console.log(result);
+
+// const favoriteFruit = ["Mango", "Banana", "Orange"];
+
+// function addFruit(fruitName) {
+//   favoriteFruit.push(fruitName);
+//   console.log(favoriteFruit.join(","));
+// }
+
+// addFruit("Apple");
+// addFruit("Jackfruit");
+
+// function calculateArea(length, width) {
+//   if (typeof length != "number" || typeof width != "number") {
+//     return "Input number is not number value.";
+//   } else {
+//     return length * width;
+//   }
+// }
+
+// let result = calculateArea(5, "10");
+// console.log(result);
+
+// function calculateSum(a, b) {
+//   if (a == null || b == null) {
+//     return "Both arguments are required";
+//   }
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     return "Both arguments must be numbers";
+//   }
+//   return a + b;
+// }
+
+// let result = calculateSum(10, 20);
+
+// console.log(result); // Output should be "Both arguments must be numbers"
+// var x = 10;
+
+// function exampleFunction() {
+//   console.log(x); // undefined
+//   var x = 20;
+//   console.log(x); // 20
+// }
+
+// exampleFunction();
+// console.log(x); // 10
+
+// function convertToNumber(input) {
+//   try {
+//     return Number(input);
+//   } catch (error) {
+//     return NaN;
+//   }
+// }
+
+// console.log(convertToNumber("nothing"));
