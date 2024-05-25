43fa31975d65552f4afc23c63e31df90813c4f74
Maruf Robin <marufrobin5@gmail.com>
2024-05-25 08:46:22 +0600

    function on going

--- JavaScript Learning/test.js
+++ JavaScript Learning/test.js
@@ -70,3 +70,79 @@
 // }
 
 // console.log(convertToNumber("nothing"));
+
+// function calculateSum(arrayOfNumber) {
+//   try {
+//     if (typeof arrayOfNumber == "object") {
+//       console.log(typeof arrayOfNumber);
+
+//       let result = 0;
+//       arrayOfNumber.forEach((element) => {
+//         result += element;
+//       });
+//       console.log(result);
+//     }
+//   } catch (error) {
+//     console.error("Error hoise");
+//   }
+// }
+// calculateSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// let double = (num) => num * 2;
+
+// console.log(double(6));
+
+// function createCounter() {
+//   let counter = 0;
+//   return function counter(params) {
+//     return ++counter;
+//   };
+// }
+
+// console.log(createCounter().counter());
+
+// function greet(name) {
+//   console.log(`Hello, ${name}`);
+// }
+
+// greet("Alice"); // Output should be "Hello, Alice"
+
+// function getName(name = "Robin") {
+//   return `My name is ${name}`;
+// }
+
+// console.log(getName());
+
+// function repeat(fn, n) {
+//   return n * fn;
+// }
+
+// const repeatVariable = repeat(function () {}, 2);
+// console.log(repeatVariable);
+
+// function delay() {
+//   return setTimeout(() => {
+//     console.log("Robin");
+//   }, 1500);
+// }
+
+// delay();
+
+// function debounce(fn, delayTime) {
+//   return setTimeout(() => {
+//     fn();
+//   }, delayTime);
+// }
+// function execute() {
+//   debounce(function () {
+//     console.log("My name");
+//   }, 1300);
+// }
+// execute();
+
+// function multiply(a, b) {
+//   return Number(a) * Number(b);
+// }
+
+// let product = multiply(5, "3");
+// console.log(product); // Output should be 15
